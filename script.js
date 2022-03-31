2beef16008d59a8c96e43328bf3625ad3a329bc6
kmiller <kmmiller418@gmail.com>
2022-03-31 17:38:01 -0400

    Adjust button event listener, update page content

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('.todo-list');
 
-function add() {
-    const newItem = document.createElement('li');
-    newItem.class = "todo-item";
-    newItem.innerText = `${input.value}`;
-    list.appendChild(newItem);
-}
+const newItem = document.createElement('li');
+newItemStuff = document.querySelector('input').value;
+console.log(newItemStuff);
+newItem.innerHTML = newItemStuff;
+
+list.append(newItem);
+
+button.addEventListener('click', function() {
+    const input = document.querySelector('input');
+    newItem.innerHTML = `${input.value}`;
+    if (input.value !== ''){
+        const newItem = document.createElement('li');
+        list.append(newItem);
+    } else {
+        alert("can't add an empty task!")
+    }
+})
